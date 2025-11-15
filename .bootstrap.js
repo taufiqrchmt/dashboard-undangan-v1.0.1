024ab1d8e82ddce125183f85716ad88143ec7fbb
Vibesdk <vibesdk-bot@cloudflare.com>
2025-11-15 07:50:11 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -77,7 +77,9 @@ function updateWranglerJsonc() {
 }
 
 function runSetupCommands() {
-    const commands = [];
+    const commands = [
+    "bun add @supabase/supabase-js@^2.43.0"
+];
     
     if (commands.length === 0) {
         console.log('⊘ No setup commands to run');
